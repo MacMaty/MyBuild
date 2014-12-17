d1ebe2c3a4c7fbde5ce8c5153a083975f951ff97
MacMaty <mat.boyer95@gmail.com>
2014-12-17 15:11:59 +0100

    Projet Initial

---
+++ MyBuild/BO/Type.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild.BO
+{
+    class Type
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+
+        public Type()
+        {
+
+        }
+    }
+}
