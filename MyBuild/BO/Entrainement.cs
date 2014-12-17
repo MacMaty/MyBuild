a0efe26ff9e2962375ef9d3c97359af12f9a7c7f
MacMaty <mat.boyer95@gmail.com>
2014-12-17 15:14:38 +0100

    Revert "Revert "Projet Initial""
    This reverts commit 1f5e95219fbd2a2508d11375505db003ce99c5a0.

---
+++ MyBuild/BO/Entrainement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild.BO
+{
+    class Entrainement
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+        public int Recompense { get; set; }
+        public Type LeType { get; set; }
+        private List<Exercice> lesExos = null;
+        
+        public Entrainement()
+        {
+            lesExos = new List<Exercice>();
+            
+        }
+
+       
+    }
+}
---
+++ MyBuild/BO/Equipement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild.BO
+{
+    class Equipement
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+
+        public Equipement()
+        {
+
+        }
+    }
+}
